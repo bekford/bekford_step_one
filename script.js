6b7f7180432e784ca05526cf9fca4b9138b17f39
bekford <bekford69@gmail.com>
2019-09-06 17:52:32 -0700

    this is my first exersises

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 'use strict';
 
-//alert("Hello world");
-//console.log("Hello world");
 
-let answer = confirm("Where are you?");
+var money,
+    time,
+    costs,
+    things,
+    oneDay,
+    message;
 
-console.log(answer);
+var insome = {};
+var savings = false;
+    
+money = prompt("How is your month budget?");
+//time = prompt("Введите дату в формате YYYY-MM-DD");
+costs = prompt("Введите обязательную статью расходов в этом месяце");
+things = prompt("Во сколько это обойдется?");
+
+var appData =  {"Бюджет": money,
+                "Данные времени": time,
+                "Обязательные расходы": expenses,
+                "Необязательные расходы": optionalExpenses,
+                "Доп. доход": insome,
+                "Свойство": savings};
+var expenses = {"Статья расходов": costs, "Обойдется в": things};
+
+oneDay = money/30;
+//message = "Бюджет на один день:" + oneDay;
+alert("Бюджет на один день: " + oneDay);
+console.log(expenses);
